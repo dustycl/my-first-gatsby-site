cf6bc1938c2fa5927a22c3321c3a2e1866a9eaf8
Dustin Lamperts <dustycl@gmail.com>
2021-06-01 20:21:10 -0700

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi! My name is Dustin. I am a product designer and front-end developer. I live in San Diego California and my favorite food is breakfast burritos</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
